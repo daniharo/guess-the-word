3fc390fe5210e1b74d53dd148974be1c29ec1ad3
Daniel Haro Contreras <daniel@appfluence.com>
2023-04-28 10:31:33 +0200

    Imita Bot

--- mod.ts
+++ mod.ts
@@ -1,8 +1,29 @@
-import { Bot, webhookCallback } from "grammy/mod.ts";
+import {
+  Bot,
+  session,
+  webhookCallback,
+  type Context,
+  type SessionFlavor,
+} from "grammy/mod.ts";
 import { Message } from "grammy/types.ts";
 import { serve } from "http/server.ts";
-import { OpenAI } from "https://esm.sh/openai-streams@^5.1.1";
+import {
+  OpenAI,
+  type ChatCompletionRequestMessage,
+} from "https://esm.sh/openai-streams@^5.1.1";
 import "dotenv/load.ts";
+import { freeStorage } from "https://deno.land/x/grammy_storages@v2.2.0/free/src/mod.ts";
+
+type SessionData = {
+  imitating: string | null;
+  messages: ChatCompletionRequestMessage[];
+};
+
+function initial(): SessionData {
+  return { imitating: null, messages: [] };
+}
+
+type MyContext = Context & SessionFlavor<SessionData>;
 
 const BOT_TOKEN = Deno.env.get("BOT_TOKEN");
 
@@ -10,27 +31,46 @@ if (!BOT_TOKEN) {
   throw new Error("BOT_TOKEN is not set");
 }
 
-const bot = new Bot(BOT_TOKEN);
+const bot = new Bot<MyContext>(BOT_TOKEN);
+
+bot.use(
+  session({
+    initial,
+    storage: freeStorage<SessionData>(bot.token),
+  })
+);
 
 const DECODER = new TextDecoder();
 
 // Handle the /start command.
 bot.command("start", (ctx) =>
-  ctx.reply("Hey! Send me a message in any language and I'll correct it 🥸")
+  ctx.reply("Hey! Use the command /imitate <someone> to start 🤩")
 );
+
+bot.command("imitate", async (ctx) => {
+  const who = ctx.match;
+  ctx.session.imitating = who;
+  ctx.session.messages = [
+    {
+      role: "system",
+      content: `From now on, you are ${who}. You have to talk and act as if you are ${who}.`,
+    },
+  ];
+  await ctx.reply(`I'm now imitating ${who}`);
+});
+
 // Handle text messages.
 bot.on("message:text", async (ctx) => {
   await ctx.replyWithChatAction("typing");
+  const who = ctx.session.imitating;
+  if (!who) {
+    await ctx.reply("You have to use /imitate first");
+    return;
+  }
+  const messages = ctx.session.messages;
   const stream = await OpenAI("chat", {
     model: "gpt-3.5-turbo",
-    messages: [
-      {
-        role: "system",
-        content:
-          "You are a spell and grammar checker. Given a prompt, reply only with the complete corrected prompt in the same language. Do not add clarification if the prompt is already correct.",
-      },
-      { role: "user", content: ctx.message.text },
-    ],
+    messages: [...messages, { role: "user", content: ctx.message.text }],
     max_tokens: 500,
   });
   let res = "";
@@ -50,6 +90,11 @@ bot.on("message:text", async (ctx) => {
       await ctx.api.editMessageText(message.chat.id, message.message_id, res);
     }
   }
+  ctx.session.messages = [
+    ...messages,
+    { role: "user", content: ctx.message.text },
+    { role: "assistant", content: res },
+  ];
 });
 
 if (Deno.env.get("DEV") === "true") {
